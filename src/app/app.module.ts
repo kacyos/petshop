30836f85a5923a1525e6f056c8fb3d78b7707f9b
Cacio Ferreira <cacio.castro@gmail.com>
2023-07-20 17:28:38 -0300

    alteração do nome de da página pets-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { PetsPageComponentComponent } from './pets-page-component/pets-page-component.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { ResetPasswordPageComponent } from "./reset-password-page/reset-password-page.component";
+import { SignupPageComponent } from "./signup-page/signup-page.component";
+import { PetsPageComponent } from "./pets-page/pets-page.component";
 
 @NgModule({
   declarations: [
@@ -16,13 +16,10 @@ import { PetsPageComponentComponent } from './pets-page-component/pets-page-comp
     LoginPageComponent,
     ResetPasswordPageComponent,
     SignupPageComponent,
-    PetsPageComponentComponent
+    PetsPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pets-page/pets-page.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { PetsPageComponent } from "./pets-page.component";
+
+describe("PetsPageComponent", () => {
+  let component: PetsPageComponent;
+  let fixture: ComponentFixture<PetsPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [PetsPageComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PetsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
